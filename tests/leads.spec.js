3df07a1fc054203cc3a3b5dd1762ffee3b681555
Matheus Koyama <koyamaCRS@gmail.com>
2026-06-01 14:43:38 -0300

    Validando comportamentos de excecao

--- tests/leads.spec.js
+++ tests/leads.spec.js
@@ -28,7 +28,7 @@ test('deve cadastrar um lead na fila de espera', async ({ page }) => {
 });
 
 
-test('nao deve cadastrar email incorreto', async ({ page }) => {
+test('nao deve cadastrar quando o nome não é preenchido', async ({ page }) => {
   await page.goto('http://localhost:3000');
 
   //await page.click('//button[text()="Aperte o play... se tiver coragem"]')  
@@ -46,4 +46,59 @@ test('nao deve cadastrar email incorreto', async ({ page }) => {
   await page.getByTestId('modal')
        .getByText('Quero entrar na fila!').click()
 
+});
+
+test('nao deve cadastrar email incorreto', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+
+  //await page.click('//button[text()="Aperte o play... se tiver coragem"]')  
+  await page.getByRole('button', {name: 'Aperte o play... se tiver coragem'}).click()
+
+  //checkpoint verificar se esta na tela certa
+  await expect(
+    page.getByTestId('modal').getByRole('heading')
+   ).toHaveText('Fila de espera')
+
+  await page.locator('input[name="email"]').fill('qalab@hotmail.com')
+
+  await page.getByTestId('modal').getByText('Quero entrar na fila!').click()
+
+  await expect(page.locator('.alert')).toHaveText('Campo obrigatório')
+});
+
+test('nao deve cadastrar quando o email não é preenchido', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+
+  //await page.click('//button[text()="Aperte o play... se tiver coragem"]')  
+  await page.getByRole('button', {name: 'Aperte o play... se tiver coragem'}).click()
+
+  //checkpoint verificar se esta na tela certa
+  await expect(
+    page.getByTestId('modal').getByRole('heading')
+   ).toHaveText('Fila de espera')
+
+   await page.locator('input[name="name"]').fill('matheus')
+
+   await page.getByRole('button', {name: 'Quero entrar na fila!'}).click()
+   await expect(page.locator('.alert')).toHaveText('Campo obrigatório')
+});
+
+test('nao deve cadastrar quando o nome e email não é preenchido', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+
+  //await page.click('//button[text()="Aperte o play... se tiver coragem"]')  
+  await page.getByRole('button', {name: 'Aperte o play... se tiver coragem'}).click()
+
+  //checkpoint verificar se esta na tela certa
+  await expect(
+    page.getByTestId('modal').getByRole('heading')
+   ).toHaveText('Fila de espera')
+
+
+   await page.getByRole('button', {name: 'Quero entrar na fila!'}).click()
+   
+   await expect(page.locator('.alert')).toHaveText([
+    'Campo obrigatório',
+    'Campo obrigatório'
+   ])
 });
