fe8259660b4517c5b53f281f8164b5591a25e5cc
Matheus Koyama <koyamaCRS@gmail.com>
2026-06-04 18:10:57 -0300

    Interagindo com GetByLabel
    React Select para selecionar dados especificos
    
    Captura o HTML atual da página para análise no console.

--- tests/e2e/movies.spec.js
+++ tests/e2e/movies.spec.js
@@ -1,5 +1,25 @@
-import { expect, test } from '@playwright/test'
+const { test } = require('@playwright/test')
 
+const { LoginingPage } = require('../pages/LoginPage.js')
+const { Moviespage } = require('../pages/Moviespage.js')
+const { Toast } = require('../pages/Components.js')
+
+/** @type {import('../pages/Moviespage.js').Moviespage} */
+
+let loginingPage
+let moviespage
+let toast
+
+test.beforeEach(async ({ page }) => {
+  loginingPage = new LoginingPage(page)
+  toast = new Toast(page)
+  moviespage = new Moviespage(page)
+  await loginingPage.visit()
+})
+ 
 test('deve poder cadastrar um novo filme', async({page}) => {
-    
+    await loginingPage.submit('admin@zombieplus.com','pwd123')
+    await moviespage.isLoggedIn()
+
+    await moviespage.create('Nome do filme','Sinopse do filme', 'Fox Entertainment', '1972')
 })
--- tests/pages/Moviespage.js
+++ tests/pages/Moviespage.js
@@ -1,7 +1,15 @@
 import { test, expect } from '@playwright/test';
-
+                 
+       /**
+ * Captura o HTML atual da página para análise no console.
+ * Útil para identificar seletores específicos que não foram encontrados durante a execução.
+ *
+ * const html = await this.page.content();
+ * console.log(html);
+ */
 export class Moviespage {
     
+     /** @param {import('@playwright/test').Page} page */
      constructor(page){
         this.page= page
     }
@@ -11,5 +19,24 @@ export class Moviespage {
         await this.page.waitForLoadState('networkidle')
         await expect(this.page).toHaveURL(/.*admin/)
       }
+
+     async create(title,overview,company,release_year){
+      // Seletor com reger o mesmo que a[href="/admin/movies/register"]
+       await this.page.locator('a[href$="register"]').click()
+
+       await this.page.getByLabel('Titulo do filme').fill(title)
+       await this.page.getByLabel('Sinopse').fill(overview)
+
+       await this.page.locator('#select_company_id  .react-select__indicator')
+                 .click()
+
+       await this.page.locator('.react-select__option').filter({hasText: company})
+                 .click()
+
+       await this.page.locator('#select_year .react-select__indicator').click()   
+      
+       await this.page.locator('.react-select__option').filter({hasText: release_year})
+                 .click()
+     }
     
 }
